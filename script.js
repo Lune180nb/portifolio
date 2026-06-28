edcdba44e05d943a35292e55ea454af729bb07ab
Lune Souza <lunesouza2@gmail.com>
2026-06-28 18:11:52 -0300

    atualização do protifolio

--- script.js
+++ script.js
@@ -2,12 +2,18 @@
 let btnNav = document.getElementById("btn-nav")
 let btnVoltar = document.getElementById("btn-voltar")
 let responsividade = document.getElementById("menu-responsivo")
+let overlay = document.getElementById("overlay")
 
 function Menu(){
   btnNav.classList.toggle("desativar")
   btnVoltar.classList.toggle("ativo")
   responsividade.classList.toggle("ativo")
+  overlay.classList.toggle("ativo")
 }
+
+overlay.addEventListener("click", () => {
+  Menu()
+})
 
 btnVoltar.addEventListener("click", () => {
   Menu()
