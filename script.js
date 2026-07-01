4538fa2ebdbb8a2cd4a65ac7663af4d90c4b293d
Lune Souza <lunesouza2@gmail.com>
2026-07-01 15:27:43 -0300

    portifolio atualizado

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let btnNav = document.getElementById("btn-nav")
 let btnVoltar = document.getElementById("btn-voltar")
 let responsividade = document.getElementById("menu-responsivo")
 let overlay = document.getElementById("overlay")
+let link = document.querySelectorAll(".link")
+let header = document.querySelector("header")
+
 
 function Menu(){
   btnNav.classList.toggle("desativar")
@@ -10,6 +13,20 @@ function Menu(){
   responsividade.classList.toggle("ativo")
   overlay.classList.toggle("ativo")
 }
+
+link.forEach(i => {
+  i.addEventListener("click", () => {
+    Menu()
+  })
+})
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY > 19){
+    header.classList.add("scroll")
+  }else{
+    header.classList.remove("scroll")
+  }
+})
 
 overlay.addEventListener("click", () => {
   Menu()
